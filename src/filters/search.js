c28ab8b3c09543cec9f0c368bd523e43cfe7bba9
Ilya Mur <ilyamur@hotmail.com>
2021-09-16 06:28:20 +0300

    set products search

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,5 +1,28 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
-const setupSearch = () => {};
+const setupSearch = store => {
+  const form = getElement('.input-form');
+  const nameInput = getElement('.search-input');
+  form.addEventListener('keyup', () => {
+    const value = nameInput.value;
+    if (value) {
+      const newStore = store.filter(product => {
+        let { name } = product;
+        name = name.toLowerCase();
+        if (name.startsWith(value)) {
+          return product;
+        }
+      });
+      if (newStore) {
+        getElement('.products-container').innerHTML =
+          '<h3 class="filter-error">Sorry, no products here...</h3>';
+      } else {
+        display(newStore, getElement('.products-container'));
+      }
+    } else {
+      display(store, getElement('.products-container'));
+    }
+  });
+};
 
 export default setupSearch;
