20bcb927bd5c0ad7072013e15a21cfe0dccdba59
Ilya Mur <ilyamur@hotmail.com>
2021-09-17 13:48:19 +0300

    update amount in the cart

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -11,7 +11,7 @@ import addToCartDOM from './addToCartDOM.js';
 // set items
 
 const cartItemCountDOM = getElement('.cart-item-count');
-const cartItemsDOM = getElement('.cart-item');
+const cartItemsDOM = getElement('.cart-items');
 const cartTotalDOM = getElement('.cart-total');
 
 let cart = getStorageItem('cart');
@@ -31,29 +31,47 @@ const displayCartTotal = () => {
   cartTotalDOM.textContent = `Total : ${formatPrice(total)}`;
 };
 
+const increaseAmount = (id) => {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount + 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+
+  return newAmount;
+};
+
 export const addToCart = (id) => {
   let item = cart.find((cartItem) => cartItem.id === id);
 
   if (!item) {
     let product = findProduct(id);
-
     product = { ...product, amount: 1 };
     cart = [...cart, product];
-
     // add to the DOM
     addToCartDOM(product);
   } else {
+    const amount = increaseAmount(id);
+
+    const items = [...cartItemsDOM.querySelectorAll('.cart-item-amount')];
+    const newAmount = items.find((item) => item.dataset.id === id);
+    newAmount.textContent = amount;
   }
   displayCartItemCount();
   displayCartTotal();
   setStorageItem('cart', cart);
   openCart();
 };
+
 const displayCartItemDOM = () => {
   cart.forEach((cartItem) => {
     addToCartDOM(cartItem);
   });
 };
+
 const setupCartFunctionality = () => {};
 const init = () => {
   // display amount of cart items
